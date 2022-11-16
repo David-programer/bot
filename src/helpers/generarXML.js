8bd31536d5fd5a3d7d00b081edf87690b043d57a
Cristian David Velasquez Garcia <cristian.velasquez@lafortuna.local>
2022-11-16 16:41:06 -0500

    aAñadiendo avances

--- src/helpers/generarXML.js
+++ src/helpers/generarXML.js
@@ -7,99 +7,101 @@ async function xml( arrayExp ) {
 
     //XML
     const DatosPoliza =  
-        `<DatosPoliza>
-            <IdPoliza>${arrayExp.poliza}</IdPoliza>
+        '<DatosPoliza>'+
+            '<Mediador>'+
+                '<Email>'+arrayExp.email_agente+'</Email>'+
+                '<Nombre>'+arrayExp.nombre_agente+'</Nombre>'+
+                '<Telefono>'+arrayExp.telefono_agente+'</Telefono>'+
+            '</Mediador>'+
 
-            <FechaEfecto>${arrayExp.fecha_efecto}</FechaEfecto>
+            '<IdPoliza>'+arrayExp.poliza+'</IdPoliza>'+
 
-            <CodigoEntidad>
-                <CodigoInterno>${arrayExp.compania}</CodigoInterno>
-                <CodigoDGS>C0133</CodigoDGS>
-            </CodigoEntidad>
+            '<FechaEfecto>'+arrayExp.fecha_efecto+'</FechaEfecto>'+
 
-            <DatosRamo>
-                <DescripcionRamo>${arrayExp.ramo}</DescripcionRamo>
-            </DatosRamo>
+            '<CodigoEntidad>'+
+                '<CodigoInterno>'+arrayExp.compania+'</CodigoInterno>'+
+                '<CodigoDGS>C0133</CodigoDGS>'+
+            '</CodigoEntidad>'+
 
-            <OtrosDatos>
-                <AseguradosPoliza>
-                    <Asegurado>
-                        <Email>${arrayExp.email_asegurado}</Email>
-                        <Nombre>${arrayExp.nombre_asegurado}</Nombre>
-                    </Asegurado>
-                </AseguradosPoliza>
-            </OtrosDatos>
-        </DatosPoliza>`;
+            '<DatosRamo>'+
+                '<DescripcionRamo>'+arrayExp.ramo+'</DescripcionRamo>'+
+            '</DatosRamo>'+
+
+            '<OtrosDatos>'+
+                '<DatosTomador>'+
+                    '<nif>'+arrayExp.nif+'</nif>'+
+                    '<razonSocial>'+arrayExp.nombre+'</razonSocial>'+
+                    '<telefono></telefono>'+
+                '</DatosTomador>'+
+            '</OtrosDatos>'+
+        '</DatosPoliza>';
 
     const DatosSiniestro = 
-        `<DatosSiniestro>
-            <IdSiniestroEntidad>${arrayExp.exp}</IdSiniestroEntidad>
-            <FechaDeclaracion>${arrayExp.fecha_encargo}</FechaDeclaracion>
-            <FechaOcurrencia>${arrayExp.fecha_ocurrencia}</FechaOcurrencia>
-            <PosicionSiniestro></PosicionSiniestro>
+        '<DatosSiniestro>'+
+            '<IdSiniestroEntidad>'+arrayExp.exp+'</IdSiniestroEntidad>'+
+            '<FechaDeclaracion>'+arrayExp.fecha_encargo+'</FechaDeclaracion>'+
+            '<FechaOcurrencia>'+arrayExp.fecha_ocurrencia+'</FechaOcurrencia>'+
+            '<PosicionSiniestro></PosicionSiniestro>'+
 
-            <SituacionesSiniestro>
-                <Situacion>
-                    <NumeroOrden></NumeroOrden>
-                    <SituacionSiniestro></SituacionSiniestro>
-                    <FechaSituacion></FechaSituacion>
-                </Situacion>
-            </SituacionesSiniestro>
+            '<SituacionesSiniestro>'+
+                '<Situacion>'+
+                    '<NumeroOrden></NumeroOrden>'+
+                    '<SituacionSiniestro></SituacionSiniestro>'+
+                    '<FechaSituacion></FechaSituacion>'+
+                '</Situacion>'+
+            '</SituacionesSiniestro>'+
 
-            <DescripcionSiniestro>${arrayExp.version}</DescripcionSiniestro>
+            '<DescripcionSiniestro>'+arrayExp.version+'</DescripcionSiniestro>'+
 
-            <AccionesSiniestro>
-                <Accion>
-                    <AccionSiniestro></AccionSiniestro>
-                    <FechaAccion></FechaAccion>
-                    <DescripcionAccion></DescripcionAccion>
-                </Accion>
-            </AccionesSiniestro>
+            '<AccionesSiniestro>'+
+                '<Accion>'+
+                    '<AccionSiniestro></AccionSiniestro>'+
+                    '<FechaAccion></FechaAccion>'+
+                    '<DescripcionAccion></DescripcionAccion>'+
+                '</Accion>'+
+            '</AccionesSiniestro>'+
 
-            <LugarSiniestro>
-                <NombreVia>${arrayExp.dir}</NombreVia>
-                <CodigoPostal>${arrayExp.dir.split( ).pop()}</CodigoPostal>
-                <Poblacion>${arrayExp.poblacion}</Poblacion>
-                <Provincia>${arrayExp.provincia}</Provincia>
-            </LugarSiniestro>
+            '<LugarSiniestro>'+
+                '<NombreVia>'+arrayExp.dir+'</NombreVia>'+
+                '<Telefono>'+arrayExp.telefono_siniestro+'</Telefono>'+
+                '<Contacto>'+arrayExp.contacto_siniestro+'</Contacto>'+
+                '<Email>'+arrayExp.email_siniestro_1+' - '+ arrayExp.email_siniestro_2+'</Email>'+
+                '<CodigoPostal>'+arrayExp.dir.split( ).pop()+'</CodigoPostal>'+
+                '<Poblacion>'+arrayExp.poblacion+'</Poblacion>'+
+                '<Provincia>'+arrayExp.provincia+'</Provincia>'+
+            '</LugarSiniestro>'+
 
-            <RiesgosSiniestro>
-                <RiesgoRecSin>
-                    <DescripcionRiesgo>${arrayExp.tipo_siniestro}</DescripcionRiesgo>
-                    <Version>${arrayExp.version}</Version>
-                </RiesgoRecSin>
-            </RiesgosSiniestro>
+            '<RiesgosSiniestro>'+
+                '<RiesgoRecSin>'+
+                    '<DescripcionRiesgo>'+arrayExp.tipo_siniestro+'</DescripcionRiesgo>'+
+                    '<Version>'+arrayExp.version+'</Version>'+
+                '</RiesgoRecSin>'+
+            '</RiesgosSiniestro>'+
 
-            <tomador>
-                <nif>${arrayExp.nif}</nif>
-                <razonSocial>${arrayExp.nombre}</razonSocial>
-                <telefono></telefono>
-            </tomador>
+            '<riesgosPoliza>'+
+                '<riesgo>'+
+                    '<direccion></direccion>'+
+                '</riesgo>'+
+            '</riesgosPoliza>'+
 
-            <riesgosPoliza>
-                <riesgo>
-                    <direccion></direccion>
-                </riesgo>
-            </riesgosPoliza>
-
-            <FigurasSiniestro>
-                <FiguraSiniestro>
-                    <Tipo>${arrayExp.tipo_figura}</Tipo>
-                    <Nombre>${arrayExp.nombre_figura}</Nombre>
-                </FiguraSiniestro>
-            </FigurasSiniestro>
-        </DatosSiniestro>`;
+            '<FigurasSiniestro>'+
+                '<FiguraSiniestro>'+
+                    '<Nombre>'+arrayExp.nombre_reparador+'</Nombre>'+
+                    '<Telefono>'+arrayExp.telefono_reparador+'</Telefono>'+
+                '</FiguraSiniestro>'+
+            '</FigurasSiniestro>'+
+        '</DatosSiniestro>';
 
     const xml = 
-        `<?xml version="1.0" encoding="iso8859-1"?>
-        <ProcesosEIAC xsi:schemaLocation="http://www.tirea.es/EIAC/ProcesosEIAC ProcesosEIAC.xsd" xmlns="http://www.tirea.es/EIAC/ProcesosEIAC" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-        <Objetos>
-            <Siniestro>
-                ${DatosPoliza}
-                ${DatosSiniestro}
-            </Siniestro>
-        </Objetos>
-        </ProcesosEIAC>`;
+        '<?xml version="1.0" encoding="iso8859-1"?>'+
+        '<ProcesosEIAC xsi:schemaLocation="http://www.tirea.es/EIAC/ProcesosEIAC ProcesosEIAC.xsd" xmlns="http://www.tirea.es/EIAC/ProcesosEIAC" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'+
+        '<Objetos>'+
+            '<Siniestro>'+
+                DatosPoliza +
+                DatosSiniestro +
+            '</Siniestro>'+
+        '</Objetos>'+
+        '</ProcesosEIAC>';
 
     const filepath = `Expedientes/${arrayExp.exp} ${arrayExp.compania}.xml`;
 
@@ -113,3 +115,116 @@ async function xml( arrayExp ) {
 }
 
 module.exports = { xml };
+
+//Cambios antiguos
+// async function xml( arrayExp ) {
+
+//     // var data_array_exp =    [ 'exp','dir', 'compania', 'fecha_encargo', 'obsevaciones', 'tipo_siniestro', 'poliza', 'ramo','causa_siniestro', 'version', 'fecha_ocurrencia', 'fecha_efecto', 'nif', 'nombre' ];   
+//     // causa_siniestro:: No requerido
+
+//     //XML
+//     const DatosPoliza =  
+//         '<DatosPoliza>'+
+//             '<IdPoliza>'+arrayExp.poliza+'</IdPoliza>'+
+
+//             '<FechaEfecto>'+arrayExp.fecha_efecto+'</FechaEfecto>'+
+
+//             '<CodigoEntidad>'+
+//                 '<CodigoInterno>'+arrayExp.compania+'</CodigoInterno>'+
+//                 '<CodigoDGS>C0133</CodigoDGS>'+
+//             '</CodigoEntidad>'+
+
+//             '<DatosRamo>'+
+//                 '<DescripcionRamo>'+arrayExp.ramo+'</DescripcionRamo>'+
+//             '</DatosRamo>'+
+
+//             '<OtrosDatos>'+
+//                 '<AseguradosPoliza>'+
+//                     '<Asegurado>'+
+//                         '<Email>'+arrayExp.email_asegurado+'</Email>'+
+//                         '<Nombre>'+arrayExp.nombre_asegurado+'</Nombre>'+
+//                     '</Asegurado>'+
+//                 '</AseguradosPoliza>'+
+//             '</OtrosDatos>'+
+//         '</DatosPoliza>';
+
+//     const DatosSiniestro = 
+//         '<DatosSiniestro>'+
+//             '<IdSiniestroEntidad>'+arrayExp.exp+'</IdSiniestroEntidad>'+
+//             '<FechaDeclaracion>'+arrayExp.fecha_encargo+'</FechaDeclaracion>'+
+//             '<FechaOcurrencia>'+arrayExp.fecha_ocurrencia+'</FechaOcurrencia>'+
+//             '<PosicionSiniestro></PosicionSiniestro>'+
+
+//             '<SituacionesSiniestro>'+
+//                 '<Situacion>'+
+//                     '<NumeroOrden></NumeroOrden>'+
+//                     '<SituacionSiniestro></SituacionSiniestro>'+
+//                     '<FechaSituacion></FechaSituacion>'+
+//                 '</Situacion>'+
+//             '</SituacionesSiniestro>'+
+
+//             '<DescripcionSiniestro>'+arrayExp.version+'</DescripcionSiniestro>'+
+
+//             '<AccionesSiniestro>'+
+//                 '<Accion>'+
+//                     '<AccionSiniestro></AccionSiniestro>'+
+//                     '<FechaAccion></FechaAccion>'+
+//                     '<DescripcionAccion></DescripcionAccion>'+
+//                 '</Accion>'+
+//             '</AccionesSiniestro>'+
+
+//             '<LugarSiniestro>'+
+//                 '<NombreVia>'+arrayExp.dir+'</NombreVia>'+
+//                 '<CodigoPostal>'+arrayExp.dir.split( ).pop()+'</CodigoPostal>'+
+//                 '<Poblacion>'+arrayExp.poblacion+'</Poblacion>'+
+//                 '<Provincia>'+arrayExp.provincia+'</Provincia>'+
+//             '</LugarSiniestro>'+
+
+//             '<RiesgosSiniestro>'+
+//                 '<RiesgoRecSin>'+
+//                     '<DescripcionRiesgo>'+arrayExp.tipo_siniestro+'</DescripcionRiesgo>'+
+//                     '<Version>'+arrayExp.version+'</Version>'+
+//                 '</RiesgoRecSin>'+
+//             '</RiesgosSiniestro>'+
+
+//             '<tomador>'
+//                 '<nif>'+arrayExp.nif+'</nif>'+
+//                 '<razonSocial>'+arrayExp.nombre+'</razonSocial>'+
+//                 '<telefono></telefono>'+
+//             '</tomador>'+
+
+//             '<riesgosPoliza>'+
+//                 '<riesgo>'+
+//                     '<direccion></direccion>'+
+//                 '</riesgo>'+
+//             '</riesgosPoliza>'+
+
+//             '<FigurasSiniestro>'+
+//                 '<FiguraSiniestro>'+
+//                     '<Tipo>'+arrayExp.tipo_figura+'</Tipo>'+
+//                     '<Nombre>'+arrayExp.nombre_figura+'</Nombre>'+
+//                 '</FiguraSiniestro>'+
+//             '</FigurasSiniestro>'+
+//         '</DatosSiniestro>';
+
+//     const xml = 
+//         '<?xml version="1.0" encoding="iso8859-1"?>'+
+//         '<ProcesosEIAC xsi:schemaLocation="http://www.tirea.es/EIAC/ProcesosEIAC ProcesosEIAC.xsd" xmlns="http://www.tirea.es/EIAC/ProcesosEIAC" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'+
+//         '<Objetos>'+
+//             '<Siniestro>'+
+//                 DatosPoliza +
+//                 DatosSiniestro
+//             '</Siniestro>'+
+//         '</Objetos>'+
+//         '</ProcesosEIAC>';
+
+//     const filepath = `Expedientes/${arrayExp.exp} ${arrayExp.compania}.xml`;
+
+//     fs.writeFile(filepath, xml, (err) => {
+//         if (err) throw err;
+//         console.log("The file was succesfully saved!");
+//     })
+    
+//     return { status: 200, message: "Se ha generado el archivo con exito", data: xml  }
+//     // <Tipo>${arrayExp.datos_intervinientes_reparador}</Tipo>
+// }
--- src/helpers/scrapers/scraper_sinexia.js
+++ src/helpers/scrapers/scraper_sinexia.js
@@ -79,32 +79,40 @@ async function buscarExpediente(browser, page, exp, fecha_encargo){
 async function listaExp( dato, user, pass ){
     return (async () => {
         // inicio navegador
-        const browser = await chromium.launch( { ignoreHTTPSErrors: true, headless: false } );    //abre el navegador con las propiedades descritas 
+        const browser = await chromium.launch( { ignoreHTTPSErrors: true, headless: true } );    //abre el navegador con las propiedades descritas 
         const context = await browser.newContext(); // usa el modo incognito
         const page = await context.newPage();   //abre una nueva paguina del navegado
+       
+        try{
+            //Loguin
+            const response = await login( browser, page, dato, user, pass)
+            if(response.status != 200){return await browser.close()}
+    
+            frameObject = page.frame({ name: 'fraPrincipal' });
 
-        const response = await login( browser, page, dato, user, pass)
-        if(response.status != 200){return await browser.close()}
-
-        frameObject = page.frame({ name: 'fraPrincipal' });
-
-        await frameObject.locator('img[title="Expedientes Nuevos"]').click();
-        await sleep(3000);
-
-        frameObject = page.frame({ name: 'fraPrincipal' });
-
-        const lista = await frameObject.$$eval('#gridbox > div.objbox > table > tbody > tr > td:nth-child(1)', (spans) => spans.map((span) => span.textContent));
-        const lista_fecha = await frameObject.$$eval('#gridbox > div.objbox > table > tbody > tr > td:nth-child(10)', (spans) => spans.map((span) => span.textContent));
-
-        let resultados = lista.map((item, index) => {return {numero_siniestro: item, fecha_encargo: lista_fecha[index] }})
-
-        if( resultados.length == 0 ){
+            try{
+                await frameObject.locator('#mostrarNotificaciones').click();
+                await sleep(1000);
+            }catch{}
+    
+            await frameObject.locator('body > table:nth-child(23) > tbody > tr:nth-child(2) > td > fieldset:nth-child(4) > table > tbody > tr:nth-child(1) > td:nth-child(2) > img').click();
+            await sleep(3000);
+    
+            frameObject = page.frame({ name: 'fraPrincipal' });
+    
+            const lista = await frameObject.$$eval('#gridbox > div.objbox > table > tbody > tr > td:nth-child(1)', (spans) => spans.map((span) => span.textContent));
+            const lista_fecha = await frameObject.$$eval('#gridbox > div.objbox > table > tbody > tr > td:nth-child(10)', (spans) => spans.map((span) => span.textContent));
+    
+            let resultados = lista.map((item, index) => {return {numero_siniestro: item, fecha_encargo: lista_fecha[index] }})
+    
+            if( resultados.length == 0 ){
+                await browser.close();
+                return { status: 200,  message: "No hay expedientes nuevos", data: [] }
+            }
+    
             await browser.close();
-            return { status: 200,  message: "No hay expedientes nuevos", data: [] }
-        }
-
-        await browser.close();
-        return { status: 200, message: "La consulta ha sido exitosa", data: resultados  }
+            return { status: 200, message: "La consulta ha sido exitosa", data: resultados  }
+        }catch(error){return { status: 500, message: "¡Lo sentimos!, ha ocurrido un error en el servidor", data: resultados  }; await browser.close()}
     })();
 }
 
@@ -112,7 +120,7 @@ async function listaExp( dato, user, pass ){
 async function XMLexp( dato, user, pass, exp, fecha_encargo ){
     return (async () => {
         // Inicio navegador
-        const browser = await chromium.launch( { ignoreHTTPSErrors: true, headless: false } );    //abre el navegador con las propiedades descritas 
+        const browser = await chromium.launch( { ignoreHTTPSErrors: true, headless: true } );    //abre el navegador con las propiedades descritas 
         const context = await browser.newContext(); // usa el modo incognito
         const page = await context.newPage();   //abre una nueva paguina del navegado
 
@@ -137,9 +145,10 @@ async function XMLexp( dato, user, pass, exp, fecha_encargo ){
                 version: await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(15) > table > tbody > tr > td').textContent(),
                 nif: await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(9) > table > tbody > tr:nth-child(8) > td:nth-child(2)').textContent(),
                 fecha_efecto : await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(9) > table > tbody > tr:nth-child(2) > td:nth-child(4)').textContent(),
-                email_asegurado: await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(9) > table > tbody > tr:nth-child(2) > td:nth-child(4)').textContent(),
+                email_agente: await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(12) > table > tbody > tr:nth-child(3) > td:nth-child(4)').textContent(),
                 datos_intervinientes_reparador : await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(11) > table > tbody > tr:nth-child(7) > td.valorCampo').textContent(),
-                nombre_asegurado: await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(8) > table > tbody > tr:nth-child(1) > td.valorCampo').textContent(),
+                nombre_agente: await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(12) > table > tbody > tr:nth-child(2) > td:nth-child(2)').textContent(),
+                telefono_agente: await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(12) > table > tbody > tr:nth-child(2) > td:nth-child(4)').textContent(),
                 provincia: await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(8) > table > tbody > tr:nth-child(3) > td:nth-child(2)').textContent(),
                 tipo_figura:  await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(6) > table > tbody > tr:nth-child(3) > td:nth-child(1) > a').textContent(),
                 nombre_figura:  await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(6) > table > tbody > tr:nth-child(3) > td:nth-child(2) > a').textContent(),
@@ -149,6 +158,12 @@ async function XMLexp( dato, user, pass, exp, fecha_encargo ){
                 dir: await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(7) > table > tbody > tr:nth-child(1) > td:nth-child(2)').textContent(),
                 codigo_postal: await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(7) > table > tbody > tr:nth-child(1) > td:nth-child(4)').textContent(),
                 ramo : await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(9) > table > tbody > tr:nth-child(1) > td:nth-child(2)').textContent(),
+                nombre_reparador : await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(12) > table > tbody > tr:nth-child(1) > td:nth-child(2)').textContent(),
+                telefono_reparador : await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(12) > table > tbody > tr:nth-child(1) > td:nth-child(4)').textContent(),
+                email_siniestro_1 : await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(7) > table > tbody > tr:nth-child(5) > td:nth-child(2)').textContent(),
+                email_siniestro_2 : await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(7) > table > tbody > tr:nth-child(4) > td:nth-child(2)').textContent(),
+                telefono_siniestro : await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(7) > table > tbody > tr:nth-child(2) > td:nth-child(4)').textContent(),
+                contacto_siniestro : await frameObject.locator('body > div.layerExpediente > form > fieldset:nth-child(7) > table > tbody > tr:nth-child(3) > td:nth-child(2)').textContent(),
             }
             
             await browser.close();
